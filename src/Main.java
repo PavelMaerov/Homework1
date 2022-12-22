261af29877c164c9c3b62e670496a1ff452150ca
asus <123456>
2022-12-23 00:10:17 +0300

    Cделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     // Важные изменения
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет, мир!");
     }
 }
